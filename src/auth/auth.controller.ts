0945195c941f98a7d926fb9d3bf1ac62d10652b8
jeosafa eller <jeosafaeller@gmail.com>
2024-03-19 07:32:41 -0300

    globalpipes feat

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
+import { AuthDto } from "src/dto";
 
 //o caminho para o controlador é /auth
 @Controller('auth')
@@ -7,7 +8,7 @@ export class AuthController{
 constructor(private authService:AuthService){}
 
 @Post('signup')
-signup(){
+signup(@Body() dto:AuthDto){
     return this.authService.signup();
 }
 
